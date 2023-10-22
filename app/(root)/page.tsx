c52bc912c8892857f753a25e25c1dcd0967980d7
Phornlapat <phornlapat.p@gmail.com>
2023-10-22 12:37:48 +0700

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import ThreadCard from "@/components/cards/ThreadCard";
+import Pagination from "@/components/shared/Pagination";
+
+import { fetchPosts } from "@/lib/actions/thread.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
+
+async function Home() {
+  return <main>hi</main>;
+}
+export default Home;
